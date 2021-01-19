7fdf8d955042724506e15eeac6c4c0926ff80499
ShirleyLoss <41438312+ShirleyLoss@users.noreply.github.com>
2021-01-19 16:42:18 -0500

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,1 +1,24 @@
+public class Hello{
+  
+  public static void main(String[] args){
+    System.out.println("Hello World");
+  }
+  
+}
 
+/* Java keywords: have a specific meaning and are case sensitive. Eg. public, Public and PUBLIC are different things.
+ * public is Java keyword. It is an access modifier. 
+ *    An access modifier allows us to define the scope or how other parts of your code or even some else's code can access this code - more on that later.
+ * class is Java keyword. Used to define a class with the name following the keyword - Hello(in this case) and left and right curly braces to define the class block.
+ * static is Java keyword that needs an understanding of other concepts.
+ *    for now, know that we need to hace static for java to find our method to run the code we are going to add.
+ * void is Java keyword. Used to indicate that the method will not return anything - more on that later.
+ **/
+
+/* Left and right parenthesis in a method declaration are mandatory and can optionally include one or more parameters 
+ *    - which is a way to pass information to a method - more on that later.
+ **/
+
+// Code block - Define a clock of code. It's mandatory to have one in a method declaration and it's here where we will be adding statements to perform certain tasks.
+
+// Statement - A complete command to be executed and can include one or more expressions.
