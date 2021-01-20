71e911c15f36c17e4d2021f8c46d2de5ebb45acc
ShirleyLoss <41438312+ShirleyLoss@users.noreply.github.com>
2021-01-20 13:03:59 -0500

    Create startingOutWithExpression.java

---
+++ startingOutWithExpression.java
@@ -0,0 +1,15 @@
+
+public class startingOutWithExpression{
+  
+  public static void main(String[] args){
+    System.out.println("Hello World");
+    
+    int myFirstNumber = 5 + 10 + 2 * 10;
+    int mySecondNumber = 12;
+    int myThirdNumber = 6;
+    int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+    System.out.println(myTotal);
+    
+  }
+  
+}
