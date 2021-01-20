dd58bb60a0eea55bd0354b27a12a025cb3908718
ShirleyLoss <41438312+ShirleyLoss@users.noreply.github.com>
2021-01-20 01:54:51 -0500

    Create Variables.java

---
+++ Variables.java
@@ -0,0 +1,17 @@
+public class Variables{
+  
+  public static void main(String[] args){
+    System.out.println("Hello World");
+    
+    int myFirstNumber = 5;
+    System.out.println(myFirstNumber);
+    
+  }
+  
+}
+
+/* Data types are keyword in Java
+ * To define a variable we need to specify the data type, then give our variable a name, and optionally add an expression to initialize. the variable with a value.
+ * Declaration Statement: Used to define a variable by indicating the data type, and the name, and optionally to set the variable to a certain value.
+ *    Eg. int myFirstNumber = 5;
+ * Expression: A construct that evaluates to a single value - more on this later.
